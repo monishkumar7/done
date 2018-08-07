b3bf67aa5809cf0dbc5d3eaeb0f20759a7b7de8c
monishkumar <monishkumar1154@gmail.com>
2018-08-07 16:24:39 -0400

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import reducer from "./store/reducers/reducer";
+
+const store = createStore(reducer);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  stateObject: "Hello World"
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
