78ea2f0bffe69531e1f669a802b9c585d22fa9ad
monishkumar <monishkumar1154@gmail.com>
2018-08-07 16:39:47 -0400

    Setup thunk middleware and Redux devtools

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
@@ -11,7 +12,9 @@ import registerServiceWorker from "./registerServiceWorker";
 
 import reducer from "./store/reducers/reducer";
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
